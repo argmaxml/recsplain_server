885c00403b3b50ca010acfced00bd6c574649480
urigoren <ugoren@gmail.com>
2022-07-27 12:37:46 +0300

    git update

--- recserver/encoder_logic.go
+++ recserver/encoder_logic.go
@@ -385,6 +385,7 @@ func (schema Schema) partition_number(query map[string]string, variant string) i
 	}
 	filters[0] = variant
 	partition_key := strings.Join(filters, "~")
+	fmt.Print("Partition key: '", partition_key, "'")
 	partition_idx, found := schema.PartitionMap[partition_key]
 	if !found {
 		// No partition found, try manually searching for it
